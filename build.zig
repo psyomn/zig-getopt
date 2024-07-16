d220a4bd55a07bb16c5b3c8e14108c9c3eaccccf
Simon Symeonidis <lethaljellybean@gmail.com>
2024-07-15 20:33:51 -0400

    zig-0.13.0: update build.zig incantations

--- build.zig
+++ build.zig
@@ -1,17 +1,15 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("getopt", .{
-        .source_file = .{
-            .path = "getopt.zig",
-        },
+        .root_source_file = b.path("getopt.zig"),
     });
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "getopt.zig" },
+        .root_source_file = b.path("getopt.zig"),
         .target = target,
         .optimize = optimize,
     });
